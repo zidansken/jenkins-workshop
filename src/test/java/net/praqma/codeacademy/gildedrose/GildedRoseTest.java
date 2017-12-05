c7f0802b2e9eb55081944d126e960d10b0b8dc72
zidansken <34234307+zidansken@users.noreply.github.com>
2017-12-05 15:19:52 +0100

    Update GildedRoseTest.java

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -18,9 +18,3 @@ public class GildedRoseTest {
 
 
 
-public class LarsErHer {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, Mr. Lars");
-   }
-}
